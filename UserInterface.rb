5aace9ab76da4abc4b8009e1c9efd17fe77ba5eb
John McCollum <johnvmccollum@gmail.com>
2017-09-28 11:04:42 +0100

    Refactor UI to use guards

--- UserInterface.rb
+++ UserInterface.rb
@@ -6,20 +6,21 @@ class UserInterface
   end
 
   def show_title()
+    return if @test_mode
     puts Ascii.get_title()
   end
 
   def start_turn(player)
-    if !@test_mode
-      puts "#{player.name} is up! Press return to roll (press 'q' to quit):"
-      input = gets.chomp
-      if input.downcase == 'q'
-        exit
-      end
+    return if @test_mode
+    puts "#{player.name} is up! Press return to roll (press 'q' to quit):"
+    input = gets.chomp
+    if input.downcase == 'q'
+      exit
     end
   end
 
   def get_player_name(i)
+    return if @test_mode
     puts "Enter player #{i}'s name:"
     name = gets.chomp
 
@@ -28,30 +29,31 @@ class UserInterface
   end
 
   def roll_dice(player, move)
+    return if @test_mode
     puts "#{player.name} rolls a #{move}" if !@test_mode
   end
 
   def show_player_move(player)
+    return if @test_mode
     puts "#{player.name} moves to position #{player.position}" if !@test_mode
   end
 
   def put_modifier(player, modifier_type, new_position)
-    puts "#{player.name} has hit a #{modifier_type} and is now on position #{new_position}!" if !@test_mode
+    return if @test_mode
+    puts "#{player.name} has hit a #{modifier_type} and is now on position #{new_position}!"
   end
 
   def end_turn
-    if !@test_mode
-      puts
-      puts "===================="
-      puts
-    end
+    return if @test_mode
+    puts
+    puts "===================="
+    puts
   end
 
   def game_over(player)
-    if !@test_mode
-      puts "#{player.name} has won, and says \"#{player.celebrate}\""
-      puts Ascii.get_something
-    end
+    return if @test_mode
+    puts "#{player.name} has won, and says \"#{player.celebrate}\""
+    puts Ascii.get_something
   end
 
 end
